3138f9f65a5a982492e24ee3b85ffb456c6b5a9f
Dan Suh <kaehops@gmail.com>
2018-03-08 21:36:05 +0900

    Created input box and a button.

--- ChatRoom.js
+++ ChatRoom.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  ScrollView, Text, StyleSheet, TextInput, Button,
+} from 'react-native';
 
 const styles = StyleSheet.create({
   mainPage: {
     fontSize: 60,
+  },
+  inputBox: {
+    height: 50,
+    borderColor: 'black',
+    borderWidth: 2,
   }
 });
 
 class ChatRoom extends Component<{}> {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Placeholder' };
+  }
+
   render() {
     return (
-      <View>
+      <ScrollView>
         <Text style={styles.mainPage}>
           {"This is the\nnext\npage."}
         </Text>
-      </View>
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Button
+          title="보내기"
+          color="red"
+        />
+      </ScrollView>
     );
   }
 }
